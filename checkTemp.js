c5eabef4e5d6febcd18235fec6eca20a896c6130
Artemi.Baburin <114190381+Jaitsa@users.noreply.github.com>
2022-09-30 12:46:52 +0300

    Update and rename checkTemp.Artemi.Baburin.txt to checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,7 +1,6 @@
-{  
+
     function checkTemp(n) {
         if(Number(n) <= 20 && Number(n) >=0) return -1
         if(Number(n) >=21 && Number(n) <= 40) return 0
         if(Number(n) >=41 && Number(n) <= 60) return 1
     }
-}
